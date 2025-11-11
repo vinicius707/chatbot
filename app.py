c6d86e9ed964887c50178b35aec18d5758bc6bb0
Vinicius <viniciussilva707@gmail.com>
2025-11-10 22:41:00 -0300

    Add initial project files including HTML, CSS, JavaScript, and SVG assets for chatbot interface

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, Response
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4o-mini"
+
+app = Flask(__name__)
+app.secret_key = 'alura'
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    prompt = request.json['msg']
+    resposta = bot(prompt)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
